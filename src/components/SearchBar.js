a738242382a3272ae7373020a8c8178a0281ce88
Joey Zelinka <cheflife365@gmail.com>
2021-10-28 13:24:17 -0400

    clear

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,13 +7,40 @@ import {
 const SearchBar = ({setResults}) => {
   const [name, setName] = useState('');
   const [text, setText] = useState('');
+  const [colors, setColors]= useState('');
+  const [cmc, setCmc]= useState('');
+  const [types, setTypes]= useState('');
+  const [subtypes, setSubTypes]= useState('');
+  const [power, setPower]= useState('');
+  const [toughness, setToughness] = useState('');
+  const [rarity, setRarity]= useState('');
 
   const handleNameChange = event => {
     setName(event.target.value);
   }
-
   const handleTextChange = event => {
     setText(event.target.value);
+  }
+  const handleColorChange = event => {
+    setColors(event.target.value);
+  }
+  const handleCmcChange = event => {
+    setCmc(event.target.value);
+  }
+  const handleTypesChange = event => {
+    setTypes(event.target.value);
+  }
+  const handleSubTypesChange = event => {
+    setSubTypes(event.target.value);
+  }
+  const handlePowerChange = event => {
+    setPower(event.target.value);
+  }
+  const handleToughnessChange = event => {
+    setToughness(event.target.value);
+  }
+  const handleRarityChange = event => {
+    setRarity(event.target.value);
   }
 
   const handleSubmit = async (event) => {
@@ -26,16 +53,52 @@ const SearchBar = ({setResults}) => {
     <div id="search">
         <h3>Card Search:</h3>
         <form onSubmit={ handleSubmit }>
-          <input 
-            type="text" 
-            placeholder="card search"
-            value={ name }
-            onChange={ handleNameChange } />
-          <input 
-            type="text"
-            placeholder="card text"
-            value={ text }
-            onChange={ handleTextChange } />
+        
+        <input
+          type="text"
+          placeholder="Search by Card Name"
+          value={name}
+          onChange={handleNameChange} />
+          <input
+           type="text"
+           placeholder="Search by Color"
+           value={colors}
+           onChange={handleColorChange} />
+          <input
+          type="text"
+          placeholder="Search by Types"
+          value={types}
+          onChange={handleTypesChange} />
+           <input
+          type="text"
+          placeholder="Search by Subtypes"
+          value={subtypes}
+          onChange={handleSubTypesChange} />
+        <input
+          type="text"
+          placeholder="Search Card Text"
+          value={text}
+          onChange={handleTextChange} />
+          <input
+          type="text"
+          placeholder="Search by Power"
+          value={power}
+          onChange={handlePowerChange} />
+          <input
+          type="text"
+          placeholder="Search by Toughness"
+          value={toughness}
+          onChange={handleToughnessChange} />
+          <input
+          type="text"
+          placeholder="Search by CMC"
+          value={cmc}
+          onChange={handleCmcChange} />
+          <input
+          type="text"
+          placeholder="Search by Rarity"
+          value={rarity}
+          onChange={handleRarityChange} />
           <button type="submit">Search</button>
         </form>
     </div>
